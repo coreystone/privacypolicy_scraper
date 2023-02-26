594102882dd311e2eea7ca8a3d4e5440207d042b
coreystone <24906602+coreystone@users.noreply.github.com>
2023-02-25 16:43:52 -0800

    Removed csv, replaced with pandas for performance.

--- privacypolicy_scraper.py
+++ privacypolicy_scraper.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -17,8 +17,7 @@ def get_potential_emails(soup_string):
     elif len(emails) == 0:
         return ""
     else:
-        # csv writerow() accepts an iterable, so it will try to write each element to subsequent cells rather than into a single cell
-        return emails  # when a set of multiple elements is returned, each element is pasted into an individual cell
+        return emails
 
 
 def get_potential_web_form_vendor(soup_string) -> str:
@@ -48,7 +47,7 @@ def get_potential_web_form_vendor(soup_string) -> str:
         return ""
 
 
-def get_potential_web_form(soup) -> set(str):
+def get_potential_web_form(soup):
     """
     Attempts to scrape URL of a web form to exercise privacy rights (such as a OneTrust web form).
     Uses a regex pattern to search for known web form provider domains. Saves a set of unique links that match the pattern.
@@ -59,6 +58,7 @@ def get_potential_web_form(soup) -> set(str):
         links.append(link.get('href'))
 
     links = set(links)
+    print("Unique web forms found:", links) if (len(links) > 0) else print("Web form not found.")
     if len(links) == 1:
         return links.pop()
     elif len(links) == 0:
@@ -74,6 +74,7 @@ def check_ccpa(page_text) -> bool:
     """
     regex_ccpa = re.compile(r'(California( Consumer Privacy Act)?)|(\(?C\.?C\.?P\.?A\.?\)?)')
     regex_cpra = re.compile(r'(California( Privacy Rights Act)?)|(\(?C\.?P\.?R\.?A\.?\)?)')
+    if bool(regex_ccpa.search(page_text)) or bool(regex_cpra.search(page_text)): print("--- CCPA mentioned")
     return bool(regex_ccpa.search(page_text)) or bool(regex_cpra.search(page_text))
 
 
@@ -82,6 +83,7 @@ def check_cpa(page_text) -> bool:
     returns True or False depending on result of Colorado Privacy Act (CPA) regular expression
     """
     regex = re.compile(r'(Colorado( Privacy Act)?)|(\b\(?C\.?P\.?A\.?\)?)')
+    if bool(regex.search(page_text)): print("--- CPA mentioned")
     return bool(regex.search(page_text))
 
 
@@ -90,6 +92,7 @@ def check_ctdpa(page_text) -> bool:
     returns True or False depending on result of Connecticut Data Privacy Act (CTDPA) regular expression
     """
     regex = re.compile(r'(Connecticut( Data Privacy Act)?)|(\(?C\.?T\.?D\.?P\.?A\.?\)?)')
+    if bool(regex.search(page_text)): print("--- CTDPA mentioned")
     return bool(regex.search(page_text))
 
 
@@ -98,6 +101,7 @@ def check_cdpa(page_text) -> bool:
     returns True or False depending on result of Viriginia Consumer Data Protection Act ((V)CDPA) regular expression
     """
     regex = re.compile(r'(Virginia( Consumer Data Protection Act)?)|(\(?V?\.?C\.?D\.?P\.?A\.?\)?)')
+    if bool(regex.search(page_text)): print("--- CDPA mentioned")
     return bool(regex.search(page_text))
 
 
@@ -106,6 +110,7 @@ def check_ucpa(page_text) -> bool:
     returns True or False depending on result of Utah Consumer Privacy Act (UCPA) regular expression
     """
     regex = re.compile(r'(Utah( Consumer Privacy Act)?)|(\(?U\.?C\.?P\.?A\.?\)?)')
+    if bool(regex.search(page_text)): print("--- UCPA mentioned")
     return bool(regex.search(page_text))
 
 
@@ -121,70 +126,60 @@ if __name__ == '__main__':
 
     print("Beginning Privacy Policy Scraper...")
 
-    # Thanks to https://stackoverflow.com/a/57518881
-    with open(input_file_path, 'r', newline='') as inputFile, open(output_file_path, 'w', newline='') as writerFile:
-        read_file = csv.reader(inputFile)
-        write_file = csv.writer(writerFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        print("File opened:", input_file_path, "|", "Writing to:", output_file_path)
+    try:
+        df = pd.read_csv(input_file_path)
+        df.fillna('', inplace=True)  # Replaces all NaN values with empty string for adding values
+    except:
+        print("Couldn't read .CSV file. Check formatting and try again.")
+
+    print("File opened:", input_file_path, "|", "Writing to:", output_file_path)
+    print("---------------------------------------------------------------------------")
+
+    for row in df.itertuples():
+        # row[0]=company, row[1]=pp, row[2]=email, row[3]=webform, row[4]=webform_vendor,
+        # row[5]=ccpa, row[6]=cpa, row[7]=ctdpa, row[8]=cdpa, row[9]=ucpa, row[10]=notes
+
+        company_name       = row.Company
+        privacy_policy_url = row.PrivacyPolicy
+
+        print("[#{index}] Attempting request:".format(row.Index), company_name, "|", privacy_policy_url)
+
+        try:
+            page = requests.get(privacy_policy_url)
+
+        except:
+            print("(EXCEPTION) Error: moving to next company.")
+            print("---------------------------------------------------------------------------")
+            continue
+
+        if page.status_code == 200:
+            print("Connection successful (HTTP 200 OK). Begin parsing.")
+            soup = BeautifulSoup(page.content, features="html.parser")
+            soup_string = str(soup)
+
+            page_text = soup.get_text()
+
+            df.at[row.Index, "Company"] = company_name
+            df.at[row.Index, "PrivacyPolicy"] = privacy_policy_url
+            df.at[row.Index, "Contact"] = get_potential_emails(soup_string)
+            df.at[row.Index, "WebForm"] = get_potential_web_form(soup)
+            df.at[row.Index, "WebFormVendor"] = get_potential_web_form_vendor(soup_string)
+            df.at[row.Index, "CCPA"] = check_ccpa(page_text)
+            df.at[row.Index, "CPA"] = check_cpa(page_text)
+            df.at[row.Index, "CTDPA"] = check_ctdpa(page_text)
+            df.at[row.Index, "CDPA"] = check_cdpa(page_text)
+            df.at[row.Index, "UCPA"] = check_ucpa(page_text)
+
+        elif page.status_code == 403:  # Read: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403
+            print("Connection refused (HTTP 403 FORBIDDEN). Continuing to next company.")
+
+        elif page.status_code == 406:  # Read: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/406
+            print("Connection refused (HTTP 406 NOT ACCEPTED). Continuing to next company.")
+
+        else:
+            print("Unexpected behavior; connection failed: HTTP", page.status_code,". Continuing to next company.")
+
+        print("Output row:", row)
         print("---------------------------------------------------------------------------")
-        r = 0
-        for row in read_file:
-            # row[0]=company, row[1]=pp, row[2]=email, row[3]=webform, row[4]=webform_vendor,
-            # row[5]=ccpa, row[6]=cpa, row[7]=ctdpa, row[8]=cdpa, row[9]=ucpa, row[10]=notes
 
-            if r == 0:  # for the first row in the CSV, just copy the column headers
-                write_file.writerow(row)
-                r += 1
-                continue
-
-            else:
-                company_name = row[0]
-                privacy_policy_url = row[1]
-
-                print("Attempting request:", company_name, "|", privacy_policy_url)
-
-                try:
-                    page = requests.get(privacy_policy_url)
-
-                except:
-                    print("(EXCEPTION) Error: moving to next company.")
-                    page_output_row = (company_name, privacy_policy_url)
-                    print("Output row:", page_output_row)
-                    write_file.writerow(page_output_row)
-                    print("---------------------------------------------------------------------------")
-                    continue
-
-                if page.status_code == 200:
-                    print("Connection successful (HTTP 200 OK). Begin parsing.")
-                    soup = BeautifulSoup(page.content, features="html.parser")
-                    soup_string = str(soup)
-
-                    page_text = soup.get_text()
-
-                    page_output_row = (company_name,
-                                       privacy_policy_url,
-                                       get_potential_emails(soup_string),
-                                       "",  # to do: find and link to web form(s)
-                                       get_potential_web_form_vendor(soup_string),
-                                       check_ccpa(page_text),
-                                       check_cpa(page_text),
-                                       check_ctdpa(page_text),
-                                       check_cdpa(page_text),
-                                       check_ucpa(page_text),
-                                       )
-
-                elif page.status_code == 403:  # Read: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403
-                    print("Connection refused (HTTP 403 FORBIDDEN). Continuing to next company.")
-                    page_output_row = (company_name, privacy_policy_url)
-
-                elif page.status_code == 406:  # Read: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/406
-                    print("Connection refused (HTTP 406 NOT ACCEPTED). Continuing to next company.")
-                    page_output_row = (company_name, privacy_policy_url)
-
-                else:
-                    print("Unexpected behavior; connection failed: HTTP", page.status_code,"| Continuing to next company.")
-                    page_output_row = (company_name, privacy_policy_url)
-
-                print("Output row:", page_output_row)
-                write_file.writerow(page_output_row)
-                print("---------------------------------------------------------------------------")
+    df.to_csv(output_file_path, index=False) # Saves the result to CSV in output path
